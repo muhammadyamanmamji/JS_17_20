291b0b40984f1d0df38a09ad4790478d0b7aeeff
muhammadyamanmamji <143739066+muhammadyamanmamji@users.noreply.github.com>
2023-09-18 05:47:51 -0700

    Add files via upload

---
+++ 17 to 20/app.js
@@ -0,0 +1,31 @@
+for(var i = 0;  i <= 10;i++){
+
+        document.write(i + "<br>")
+    }
+
+    for(var i = 1; i <= 12 ; i++){
+        document.write(i+"<br>")
+    }
+
+    for (var i = 0; i <= 4; i++){}
+
+
+      for(var i = 1; i <= 100 ; i++){
+        document.write(i+"<br>")
+    }
+
+    for(var i = 7 ; i>3 ; i++){
+        document.write(i + "<br>")
+    }
+
+     var flag = true;
+debugger
+    for(var i = 0; i<10 ; i++){
+        if(i === 1){
+            alert("it should be one");
+        }
+        break;
+    }
+
+var username = ["yaman"];
+var enter_name = prompt("Enter user name")
---
+++ ja ch 1-5/app.js
@@ -0,0 +1,85 @@
+// Chap 1:Alert
+// alert("Yaman");
+
+// alert("Mamji");
+
+// alert("yammomamji55@gmail.com");
+
+// alert("03218330233");
+
+// alert("12345");
+
+// alert("You're learning JavaScript!");
+
+// alert("this is my first java script work and i am very enjoying this language");
+
+
+// Chap 2 Variables for string:
+
+
+var YamanMamji; 
+var MuhammadYamanMamji;
+
+var a;
+a="1";
+
+
+var teamName; 
+teamName ="Team name"
+// alert(teamName + ":" + " "+ "Babar Azam" + " "+ "Shoaib Mailk");
+
+var bestMan = "yaman mamji";
+
+
+
+// Chap 3 Variables for numbers:
+
+
+var caseQty;
+
+var numbers = 144;
+
+var num =9;
+
+var num2;
+num2 = 2 + 5 + 5;
+
+var pizza =100;
+var burger =50;
+var total = pizza + burger;
+
+var value = 10;
+value = 7;
+
+//  Chapter 4 Variable names Legal and Illegal
+
+var productcost = 3.45;
+
+var NameOfBand;
+
+var legally_named;
+
+var YamanMamji;
+
+// legal variables:
+
+var a_To_z;
+var $123;
+var a1;
+
+// illegal variables
+
+// var 123;
+// var -+*;
+// var  a b;
+
+
+
+// Chapter 5 Math Expression I
+
+var num = 20 % 6;
+alert(num);
+
+
+
+
